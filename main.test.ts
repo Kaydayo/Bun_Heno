b9057587570de467be242e19f65ac1a988b62037
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2023-03-11 16:47:21 +0100

    no more process envs

--- main.test.ts
+++ main.test.ts
@@ -1,3 +1,10 @@
-import { describe } from "bun:test";
+import { describe, expect, it } from "bun:test";
+import app from './src/index'
 
-describe('should run test',()=>)
+describe('should run test', () => {
+    it('should return successful response with status code 200', async () => {
+        const req = new Request('http://localhost/')
+        const res = await app.fetch(req)
+        expect(res.status).toBe(200)
+    })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { serveStatic } from 'hono/serve-static.bun';
+import {bearerAuth} from 'hono/bearer-auth'
 
 const port = parseInt(process.env.PORT) || 3000;
 
@@ -7,9 +8,17 @@ const app = new Hono();
 
 app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }));
 
-app.get("/", (c) => {
+const home = app.get("/", (c) => {
   return c.json({ message: "Hello World!" });
 });
+
+
+
+app.use('/auth/*', bearerAuth({ token: Bun.env.secretKey }))
+
+app.get('/auth/page', (c) => {
+  return c.text('authenticated')
+})
 
 console.log(`Running at http://localhost:${port}`);
 
